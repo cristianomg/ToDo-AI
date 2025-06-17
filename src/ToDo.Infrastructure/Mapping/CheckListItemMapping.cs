e3f34d8c2a89e0bda3c40dfd7883b2a51a9203f6
cristiano oliveira <cristianomg95@gmail.com>
2025-06-17 12:47:08 -0300

    fix: ajusta tipo de datas para a tabela check list item

---
+++ src/ToDo.Infrastructure/Mapping/CheckListItemMapping.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Domain.Entities;
+
+namespace ToDo.Infrastructure.Mapping
+{
+    public class CheckListItemMapping : IEntityTypeConfiguration<ChecklistItem>
+    {
+        public void Configure(EntityTypeBuilder<ChecklistItem> builder)
+        {
+            builder.ToTable("ChecklistItem");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Text)
+                .IsRequired()
+                .HasMaxLength(200)
+                .HasColumnType("varchar(200)");
+
+            builder.Property(x => x.Completed)
+                .HasDefaultValue(false)
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAt)
+              .HasColumnType("timestamp without time zone")
+              .IsRequired();
+
+            builder.Property(x => x.UpdatedAt)
+                .HasColumnType("timestamp without time zone");
+
+            builder.HasOne(x => x.Task)
+                .WithMany(x => x.Checklist)
+                .HasForeignKey(x => x.TaskId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ src/ToDo.Infrastructure/Migrations/20250617154602_Ajusta CheckListItemTable.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustaCheckListItemTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "ChecklistItem",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "ChecklistItem",
+                type: "varchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "ChecklistItem",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Completed",
+                table: "ChecklistItem",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 46, 2, 365, DateTimeKind.Unspecified).AddTicks(1264));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 46, 2, 365, DateTimeKind.Unspecified).AddTicks(1275));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 46, 2, 365, DateTimeKind.Unspecified).AddTicks(1275));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 46, 2, 365, DateTimeKind.Unspecified).AddTicks(1276));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 46, 2, 365, DateTimeKind.Unspecified).AddTicks(1277));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 46, 2, 365, DateTimeKind.Unspecified).AddTicks(1277));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 46, 2, 365, DateTimeKind.Unspecified).AddTicks(1278));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 46, 2, 365, DateTimeKind.Unspecified).AddTicks(1278));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 46, 2, 365, DateTimeKind.Unspecified).AddTicks(1279));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 46, 2, 365, DateTimeKind.Unspecified).AddTicks(1279));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "ChecklistItem",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "ChecklistItem",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "ChecklistItem",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Completed",
+                table: "ChecklistItem",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 37, 38, 958, DateTimeKind.Unspecified).AddTicks(7260));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 37, 38, 958, DateTimeKind.Unspecified).AddTicks(7272));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 37, 38, 958, DateTimeKind.Unspecified).AddTicks(7273));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 37, 38, 958, DateTimeKind.Unspecified).AddTicks(7273));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 37, 38, 958, DateTimeKind.Unspecified).AddTicks(7274));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 37, 38, 958, DateTimeKind.Unspecified).AddTicks(7274));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 37, 38, 958, DateTimeKind.Unspecified).AddTicks(7275));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 37, 38, 958, DateTimeKind.Unspecified).AddTicks(7275));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 37, 38, 958, DateTimeKind.Unspecified).AddTicks(7276));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "CreatedAt",
+                value: new DateTime(2025, 6, 17, 12, 37, 38, 958, DateTimeKind.Unspecified).AddTicks(7276));
+        }
+    }
+}
